71e2df5d233be6fe8811f1534b68ada16e99b752
Naily Ais Hanani <hananinailyais.nah@gmail.com>
2016-11-20 19:21:19 +0700

    perbaikan design 2

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/KonfigurasiActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/KonfigurasiActivity.java
@@ -3,10 +3,11 @@ package id.sch.smktelkom_mlg.project.xiirpl303132333.latihanun;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-class MainActivity extends AppCompatActivity {
+public class KonfigurasiActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hasil);
+        setContentView(R.layout.activity_konfigurasi);
     }
 }
