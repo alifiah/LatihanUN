febbde7d946daf8f3bc9f47115732a4eb84f1c0b
Naily Ais Hanani <hananinailyais.nah@gmail.com>
2016-11-20 19:39:11 +0700

    Konfigurasi

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/KonfigurasiActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/KonfigurasiActivity.java
@@ -1,7 +1,14 @@
 package id.sch.smktelkom_mlg.project.xiirpl303132333.latihanun;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import static id.sch.smktelkom_mlg.project.xiirpl303132333.latihanun.R.id.imageButtonSD;
+import static id.sch.smktelkom_mlg.project.xiirpl303132333.latihanun.R.id.imageButtonSMA;
+import static id.sch.smktelkom_mlg.project.xiirpl303132333.latihanun.R.id.imageButtonSMP;
 
 public class KonfigurasiActivity extends AppCompatActivity {
 
@@ -9,5 +16,31 @@ public class KonfigurasiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konfigurasi);
+
+        ImageButton sd = (ImageButton) findViewById(imageButtonSD);
+        sd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), menu.class);
+                startActivity(i);
+            }
+        });
+        ImageButton smp = (ImageButton) findViewById(imageButtonSMP);
+        smp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Menu2Activity.class);
+                startActivity(i);
+            }
+        });
+
+        ImageButton sma = (ImageButton) findViewById(imageButtonSMA);
+        sma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Menu3Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
