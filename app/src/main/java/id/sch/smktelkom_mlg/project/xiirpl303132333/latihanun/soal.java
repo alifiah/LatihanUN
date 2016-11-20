96ba0c4a0c910540ffb5917551b29bc8aa9c2a22
ErlinaNovita <puterierlina99@gmail.com>
2016-11-20 15:49:12 +0700

    Design Fix

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/soal.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/soal.java
@@ -3,8 +3,8 @@ package id.sch.smktelkom_mlg.project.xiirpl303132333.latihanun;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class soal extends AppCompatActivity {
-
+public class soal extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
