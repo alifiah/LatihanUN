0acdc6cf087f1f8d8dd1a0d9446a1efb1b7a49da
Naily Ais Hanani <hananinailyais.nah@gmail.com>
2016-12-03 20:48:07 +0700

    soal

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/soal_sd_bindo.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl303132333/latihanun/soal_sd_bindo.java
@@ -3,13 +3,11 @@ package id.sch.smktelkom_mlg.project.xiirpl303132333.latihanun;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class menu extends AppCompatActivity {
+public class soal_sd_bindo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
-
+        setContentView(R.layout.activity_soal_sd_bindo);
     }
 }
